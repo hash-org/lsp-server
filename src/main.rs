7a491aed72f0e26d33bdb826d568498a5dc68070
etheocharis <elias@thinkcrafty.eu>
2024-01-13 17:00:55 +0100

    Initial commit for a simple Tower based lsp server

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,30 @@
 use simple_logger::SimpleLogger;
-use tower_lsp::jsonrpc::Result;
-use tower_lsp::lsp_types::*;
-use tower_lsp::{Client, LanguageServer, LspService, Server};
+use tower_lsp::{LspService, Server};
 
-#[derive(Debug)]
-struct Backend {
-    client: Client,
-}
+use crate::server::{HashLanguageServer, LogLayer};
 
-#[tower_lsp::async_trait]
-impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult {
-            capabilities: ServerCapabilities {
-                hover_provider: Some(HoverProviderCapability::Simple(true)),
-                completion_provider: Some(CompletionOptions::default()), 
-                ..Default::default()
-            }, 
-            server_info: Some(ServerInfo{
-                name: "hash-lsp".to_string(),
-                version: Some("1.0".to_string())
-            }), 
-            ..Default::default()
-        })
-    }
+mod server;
 
-    async fn initialized(&self, _: InitializedParams) {
-        self.client
-            .log_message(MessageType::INFO, "server initialized!")
-            .await;
-    }
-
-    async fn shutdown(&self) -> Result<()> {
-        Ok(())
-    }
-
-    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
-        Ok(Some(CompletionResponse::Array(vec![
-            CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
-            CompletionItem::new_simple("Bye".to_string(), "More detail".to_string())
-        ])))
-    }
-
-    async fn hover(&self, _: HoverParams) -> Result<Option<Hover>> {
-        Ok(Some(Hover {
-            contents: HoverContents::Scalar(
-                MarkedString::String("You're hovering!".to_string())
-            ),
-            range: None
-        }))
-    }
-}
+use tower::Layer;
 
 #[tokio::main]
 async fn main() {
-    SimpleLogger::new().init().unwrap();
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .init()
+        .unwrap();
 
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    let (service, socket) = LspService::new(|client| HashLanguageServer { client });
+
+    let layer_withlog = LogLayer {};
+    let svc = layer_withlog.layer(service);
+
     log::info!("Starting server");
-    let x = Server::new(stdin, stdout, socket).serve(service).await;
-    
+
+    Server::new(stdin, stdout, socket).serve(svc).await;
+
+    log::info!("server stopped");
 }
